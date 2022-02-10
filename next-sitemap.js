133c903807f1dd11f23db3e7f5b59771ae054a4e
Filip Lipiński <filip.lipinski@outlook.com>
2022-02-10 19:55:48 +0100

    bump packages and remove server sitemaps

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,8 +2,4 @@ module.exports = {
   siteUrl: 'https://www.lcar.pl',
   priority: 1.0,
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap.xml'],
-  robotsTxtOptions: {
-    additionalSitemaps: ['https://www.lcar.pl/server-sitemap.xml'],
-  },
 };
